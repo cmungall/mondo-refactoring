262df0abd70a1ccceb17c15f5f0b8ae7aaf7006d
cmungall <cjm@berkeleybop.org>
2023-07-11 18:20:37 -0700

    views

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 SRC = ../mondo-edit.obo
 DB = sqlite:obo:mondo
+DB_PATH = $(HOME)/.data/oaklib/mondo.db
+
+load-views: views.sql
+	cat views.sql | sqlite3 $(DB_PATH) 
 
 # conservative
-mondo-ordo-only.obo: $(SRC)
-	obo-grep.pl -r 'xref: Orphanet' $(SRC) | obo-grep.pl --neg -r is_obsolete - | obo-grep.pl --neg -r 'xref: ([A-NP-Z]|OMIM)' - > $@
+#mondo-ordo-only.obo: $(SRC)
+#	obo-grep.pl -r 'xref: Orphanet' $(SRC) | obo-grep.pl --neg -r is_obsolete - | obo-grep.pl --neg -r 'xref: ([A-NP-Z]|OMIM)' - > $@
 
 # only filtered xrefs
 mondo-ordo-only.tsv:
-	runoak -i $(DB) query -q "SELECT DISTINCT subject FROM has_dbxref_statement WHERE value LIKE 'Orphanet:%' AND subject NOT IN (SELECT subject FROM has_dbxref_statement WHERE value NOT like 'Orphanet:%') AND subject LIKE 'MONDO:%' AND subject NOT IN (SELECT id FROM deprecated_node)" -O csv -o $@
+	runoak -i $(DB) query -q "SELECT DISTINCT subject FROM has_dbxref_statement WHERE value LIKE 'Orphanet:%' AND subject NOT IN (SELECT subject FROM has_dbxref_statement WHERE value NOT LIKE 'Orphanet:%' AND value NOT LIKE 'UMLS:%') AND subject LIKE 'MONDO:%' AND subject NOT IN (SELECT id FROM deprecated_node)" -O csv -o $@
 
 mondo-omim-ancestors.tsv: $(SRC)
 	runoak -i $(DB) ancestors -p i x^OMIM: -O csv -o $@
@@ -15,8 +19,8 @@ mondo-omim-ancestors.tsv: $(SRC)
 candidates.tsv: mondo-ordo-only.tsv mondo-omim-ancestors.tsv
 	runoak -i $(DB) labels .idfile $< .and .idfile mondo-omim-ancestors.tsv -O csv -o $@
 
-refactored.obo: $(SRC)
-	runoak --stacktrace -i simpleobo:$< apply-obsolete --ignore-invalid-changes [ .idfile candidates.tsv .idfile candidates-curated.txt ] .not .idfile rescue-curated.txt -o $@ 
+refactored.obo: $(SRC) candidates.tsv
+	runoak --stacktrace -i simpleobo:$< apply-obsolete --ignore-invalid-changes [ .idfile candidates.tsv .idfile candidates-curated.txt ] .not .idfile rescue-curated.txt -o $@.tmp && egrep -v 'relationship: excluded_subClassOf .*Rewired from link to' $@.tmp > $@
 
 refactored-normalized.obo: refactored.obo
 	robot convert -i $< -o $@
---
+++ views.sql
@@ -0,0 +1,92 @@
+-- is_a is subClassOf between NCs
+CREATE VIEW is_a AS
+ SELECT * FROM rdfs_subclass_of_statement
+ WHERE object NOT LIKE '_:%';
+
+-- ORDO equivs
+CREATE VIEW mondo_with_ordo AS
+ SELECT subject AS id
+ FROM has_dbxref_statement
+ WHERE value LIKE 'Orphanet:%' AND subject LIKE 'MONDO:%';
+ 
+-- OMIM equivs
+CREATE VIEW mondo_with_omim AS
+ SELECT subject AS id
+ FROM has_dbxref_statement
+ WHERE value LIKE 'OMIM:%' AND subject LIKE 'MONDO:%';
+
+-- proper is-a ancestor of a omim equiv
+DROP VIEW IF exists mondo_omim_ancestor;
+CREATE VIEW mondo_omim_ancestor AS
+ SELECT object AS id
+ FROM
+  entailed_edge
+ WHERE
+  predicate = 'rdfs:subClassOf' AND
+  subject IN (SELECT id FROM mondo_with_omim) AND
+  subject != object;
+ 
+-- class that is supported by ordo only 
+CREATE VIEW ordo_only AS
+ SELECT id
+ FROM mondo_with_ordo
+ WHERE
+  id NOT IN (SELECT subject FROM has_dbxref_statement WHERE value NOT LIKE 'Orphanet:%' AND value NOT LIKE 'UMLS:%') AND
+  id NOT IN (SELECT id FROM deprecated_node);
+
+-- obsolete is ordo only AND an omim anc
+DROP VIEW IF exists obsoletion_candidate;
+CREATE VIEW obsoletion_candidate AS
+ SELECT id
+ FROM
+  mondo_omim_ancestor
+ WHERE
+  id IN (SELECT id FROM ordo_only);
+
+-- supported is-a
+DROP VIEW IF exists is_a_annotation;
+CREATE VIEW is_a_annotation AS
+ SELECT *
+ FROM owl_axiom_annotation
+ WHERE
+  predicate='rdfs:subClassOf' AND
+  object LIKE 'MONDO:%';
+
+-- is-a with source (view)
+CREATE VIEW is_a_source_view AS
+ SELECT *
+ FROM is_a_annotation
+ WHERE annotation_predicate = 'oio:source';
+
+-- (materialized for speed)
+CREATE TABLE is_a_source AS
+ SELECT * FROM is_a_source_view;
+
+-- an is-a supported (in whole or part) by ordo
+DROP VIEW IF exists ordo_is_a;
+CREATE VIEW ordo_is_a AS
+ SELECT *
+ FROM is_a_source
+ WHERE annotation_value LIKE 'Orphanet:%';
+
+CREATE VIEW non_ordo_is_a AS
+ SELECT *
+ FROM is_a_source
+ WHERE annotation_value NOT LIKE 'Orphanet:%';
+
+-- an is-a supported only by ordo
+CREATE VIEW sole_ordo_is_a AS
+ SELECT *
+ FROM ordo_is_a AS e1
+ WHERE NOT EXISTS (SELECT subject,object FROM is_a_source AS e2 WHERE e1.subject=e2.subject AND e1.object=e2.object AND annotation_value NOT LIKE 'Orphanet:%');
+
+-- candidate for is-a removal
+CREATE VIEW ordo_is_a_candidate AS
+ SELECT DISTINCT e1.subject, e1.object, e1.annotation_value AS ordo_id, e2.object AS preferred_object
+ FROM
+  sole_ordo_is_a AS e1,
+  non_ordo_is_a AS e2
+ WHERE
+  e2.subject=e1.subject AND e2.object != e1.object AND
+  e1.object NOT IN (SELECT id FROM ordo_only);
+ 
