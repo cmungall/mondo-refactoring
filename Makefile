9de514ed99e53668aaee6b258568508fafcaeffd
cmungall <cjm@berkeleybop.org>
2023-07-13 10:05:57 -0700

    regen

--- Makefile
+++ Makefile
@@ -2,16 +2,18 @@ SRC = ../mondo-edit.obo
 DB = mondo.db
 SRC_DB_PATH = $(HOME)/.data/oaklib/mondo.db
 
+all: refactored-normalized.obo
+
 load-views: views.sql
-	cat views.sql | sqlite3 $(DB_PATH) 
+	cat views.sql | sqlite3 $(DB) 
 
 link:
 	ln -s $(SRC_DB_PATH) $(DB)
 
-candidates.tsv:
+candidates.tsv: views.sql
 	runoak -i $(DB) query -q "SELECT DISTINCT id FROM obsoletion_candidate ORDER BY id" -o $@
 
-refactored.obo: $(SRC) candidates.tsv
+refactored.obo: $(SRC) candidates.tsv views.sql
 	runoak --stacktrace -i simpleobo:$< apply-obsolete --ignore-invalid-changes [ .idfile candidates.tsv .idfile candidates-curated.txt ] .not .idfile rescue-curated.txt -o $@.tmp && egrep -v 'relationship: excluded_subClassOf .*Rewired from link to' $@.tmp | egrep -v 'is_a: MONDO:0000001 {description="Rewired from link' > $@
 
 refactored-normalized.obo: refactored.obo
--- views.sql
+++ views.sql
@@ -4,7 +4,7 @@ CREATE VIEW is_a AS
  WHERE object NOT LIKE '_:%';
 
 -- ORDO equivs
-DROP VIEW IF exists mondo_with_ordo;
+DROP VIEW IF EXISTS mondo_with_ordo;
 CREATE VIEW mondo_with_ordo AS
  SELECT DISTINCT subject AS id
  FROM has_dbxref_statement
@@ -17,7 +17,7 @@ CREATE VIEW mondo_with_omim AS
  WHERE value LIKE 'OMIM:%' AND subject LIKE 'MONDO:%';
 
 -- proper is-a ancestor of a omim equiv
-DROP VIEW IF exists mondo_omim_ancestor;
+DROP VIEW IF EXISTS mondo_omim_ancestor;
 CREATE VIEW mondo_omim_ancestor AS
  SELECT object AS id
  FROM
@@ -35,17 +35,68 @@ CREATE VIEW ordo_only AS
   id NOT IN (SELECT subject FROM has_dbxref_statement WHERE value NOT LIKE 'Orphanet:%' AND value NOT LIKE 'UMLS:%') AND
   id NOT IN (SELECT id FROM deprecated_node);
 
+CREATE VIEW in_subset AS
+ SELECT * FROM statements WHERE predicate='oio:inSubset';
+
+CREATE VIEW clingen AS
+ SELECT subject AS id
+ FROM in_subset WHERE object like '%clingen';
+
+CREATE VIEW ordo_group_of_disorders AS
+ SELECT subject AS id
+ FROM in_subset WHERE object like '%ordo_group_of_disorders';
+
+-- supported def
+DROP VIEW IF EXISTS def_annotation;
+CREATE VIEW def_annotation AS
+ SELECT *
+ FROM owl_axiom_annotation
+ WHERE
+  predicate='IAO:0000115';
+
+DROP VIEW IF EXISTS non_ordo_def_annotation;
+CREATE VIEW non_ordo_def_annotation AS
+ SELECT *
+ FROM def_annotation
+ WHERE annotation_value NOT LIKE 'Orphanet:%';
+
+DROP VIEW IF EXISTS non_ordo_or_self_def_annotation;
+CREATE VIEW non_ordo_or_self_def_annotation AS
+ SELECT *
+ FROM def_annotation
+ WHERE annotation_value NOT LIKE 'Orphanet:%' AND annotation_value NOT LIKE 'MONDO:%';
+
+
+-- rescue anything
+-- (1) marked as clingen
+-- (2) that has a non-trivial non-ordo definition annotation
+CREATE VIEW rescue AS
+ SELECT DISTINCT id
+ FROM clingen
+ UNION
+ SELECT DISTINCT subject AS id
+ FROM non_ordo_or_self_def_annotation;
+
+
 -- obsolete is ordo only AND an omim anc
-DROP VIEW IF exists obsoletion_candidate;
+DROP VIEW IF EXISTS obsoletion_candidate;
 CREATE VIEW obsoletion_candidate AS
  SELECT id
  FROM
-  mondo_omim_ancestor
+  ordo_only
  WHERE
-  id IN (SELECT id FROM ordo_only);
+  ( id IN (SELECT id FROM mondo_omim_ancestor)
+    OR
+    id IN (SELECT id FROM ordo_group_of_disorders)
+  )
+  AND
+  id NOT IN (SELECT id FROM rescue);
+
+
+
 
 -- supported is-a
-DROP VIEW IF exists is_a_annotation;
+DROP VIEW IF EXISTS is_a_annotation;
 CREATE VIEW is_a_annotation AS
  SELECT *
  FROM owl_axiom_annotation
@@ -64,7 +115,7 @@ CREATE TABLE is_a_source AS
  SELECT * FROM is_a_source_view;
 
 -- an is-a supported (in whole or part) by ordo
-DROP VIEW IF exists ordo_is_a;
+DROP VIEW IF EXISTS ordo_is_a;
 CREATE VIEW ordo_is_a AS
  SELECT *
  FROM is_a_source
